cade7216c4267cb236200c40628de02fd0c2c761
joao <joao.orosario01@gmail.com>
2024-11-07 08:40:08 -0300

    pagina front end quase acabada e iniciando o backend

---
+++ back_end/Job/src/main/java/List/Job/BackendListJobApplication.java
@@ -0,0 +1,13 @@
+package List.Job;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendListJobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendListJobApplication.class, args);
+	}
+
+}

6828cb166c74d4af403048b7b62b766b44d07e0e
joao <joao.orosario01@gmail.com>
2024-11-10 22:37:41 -0300

    end

---
+++ Job_list/dist/job-list/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-3WTUKTIN.mjs";import"./chunk-26ZQEVQH.mjs";import"./chunk-V7Q55Y2N.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
--- back_end/Job/src/main/java/List/Job/Configuration/WebConfig.java
+++ back_end/Job/src/main/java/List/Job/Configuration/WebConfig.java
@@ -8,6 +8,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:4200");  
+        registry.addMapping("/**") // Permite CORS para todos os endpoints
+                .allowedOrigins("http://localhost") // Permite requisições do domínio localhost
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // Métodos permitidos
+                .allowedHeaders("*"); // Permite qualquer cabeçalho
     }
 }

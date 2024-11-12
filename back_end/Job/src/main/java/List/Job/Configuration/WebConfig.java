0ad685d18d005ba89dea520ba9dffd1f51545da0
joao <joao.orosario01@gmail.com>
2024-11-12 20:42:46 -0300

    cors

--- back_end/Job/src/main/java/List/Job/Configuration/WebConfig.java
+++ back_end/Job/src/main/java/List/Job/Configuration/WebConfig.java
@@ -6,11 +6,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // Permite CORS para todos os endpoints
-                .allowedOrigins("http://localhost") // Permite requisições do domínio localhost
-                .allowedMethods("GET", "POST", "PUT", "DELETE") // Métodos permitidos
-                .allowedHeaders("*"); // Permite qualquer cabeçalho
+        registry.addMapping("/**")
+                .allowedOrigins("*") // Permite requisições de qualquer domínio
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Permite todos os métodos HTTP necessários
+                .allowedHeaders("*") // Permite todos os cabeçalhos
+                .allowCredentials(true); // Permite envio de credenciais (cookies, etc.) se necessário
     }
 }
